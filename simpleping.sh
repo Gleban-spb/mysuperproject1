430afb96e01190dbfb802176c72cdba20ddb9128
Gleb Drapkin <Gleb_D@mail.ru>
2022-11-20 16:21:55 +0300

    Пример условий из книги

---
+++ simpleping.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#Пингуем хост с помощью Nmap
+### Глобальные переменные ###
+#Сохраняем IP-адрес
+IP_ADDRESS=$1
+function ping_host(){
+ping_cmd=$(nmap -sn $IP_ADDRESS | grep 'Host is up' | cut -d '(' -f 1)
+}
+function print_status(){
+if [[ -z $ping_cmd ]]
+then
+echo 'Host is down'
+else
+echo 'Host is up'
+fi
+}
+ping_host
+print_status
