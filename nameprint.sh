78de268c4a2a95a6fd44c0471d212d12ebc9888f
Gleb Drapkin <Gleb_D@mail.ru>
2023-01-21 18:30:19 +0300

    Установка Docker

--- nameprint.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-read -p "Введите имя:" LASTNAME
-read -p "Введите фамилию:" FIRSTNAME
-printf "Фамилия, Имя  $FIRSTNAME $LASTNAME\n"
--- simplels.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-LS_CMD=$(ls |grep 'simple')
-printf "$LS_CMD\n"
